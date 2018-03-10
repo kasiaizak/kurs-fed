3e6ef8ab2dc0783ad3357cd88d226e6a0edaae63
Katarzyna Izak <kasik83@gmail.com>
2018-03-10 15:51:27 +0100

    js zmienne

---
+++ warsztaty/javascript/js/zmienne.js
@@ -0,0 +1,10 @@
+var imie;
+var nazwisko = "Izak";
+var wiek = 35;
+
+imie = "Katarzyna";
+
+// laczenie zmiennych
+var laczenieTekstu = "Moje imie to: " + imie + " a nazwisko to: " + nazwisko + " Mam " + wiek + " lat.";
+
+console.log(laczenieTekstu);
