3686a2a3641b292c4d2ebbfd9cb85332e82e13fd
Katarzyna Izak <kasik83@gmail.com>
2018-03-11 14:01:44 +0100

    js obiekty math

---
+++ warsztaty/javascript/js/math.js
@@ -0,0 +1,11 @@
+// Właściwości obiektu Math
+console.log(Math.E);
+console.log(Math.PI);
+console.log(Math.SQRT1_2);
+
+// Metody obiektu Math
+console.log(Math.cos(30));
+console.log(Math.random()*10);
+console.log(Math.pow(2,2));
+
+// https://developer.mozilla.org/pl/docs/Web/JavaScript/Referencje/Obiekty/Math
