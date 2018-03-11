e33c3b6791d452da19107317a6f386229d50127d
Katarzyna Izak <kasik83@gmail.com>
2018-03-11 09:23:32 +0100

    update funkcje

--- warsztaty/javascript/js/funkcje.js
+++ warsztaty/javascript/js/funkcje.js
@@ -5,8 +5,9 @@ function wyswietlWKonsoli() {
 
 //wyswietlWKonsoli();
 
-// funkcja z parametrami
 
+
+// funkcja z parametrami
 function wyswietlWKonsoliParm(imie, nazwisko) {
     console.log(imie + " " + nazwisko);
 }
@@ -18,14 +19,25 @@ wyswietlWKonsoliParm('Kinga', 'Korycka');
 wyswietlWKonsoliParm('Adrian', 'Bąk');
 */
 
+
+
 // zwracanie wartosci przez funkcję
 function obliczPodatek(kwota, procentPodatek) {
     var wynik = kwota * (procentPodatek / 100);
     return wynik.toFixed(2); // .toFixed zaokrągli do 2 miejsc po przecinku
 }
 
+/*
 var x = obliczPodatek(4999, 10);
 var y = obliczPodatek(2500, 30);
 var z = obliczPodatek(3699, 23);
 
 console.log("Wysokość obliczonego podatku wynosi: " + x, y, z);
+*/
+
+function wyswietlWynik(wartosc) {
+    document.write(wartosc);
+}
+
+var obliczenia = obliczPodatek(1000, 10);
+wyswietlWynik (obliczenia);
