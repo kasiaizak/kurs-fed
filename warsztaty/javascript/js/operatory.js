73fa92f370c583e4994f3f4d7a097fc0f9ef652f
Katarzyna Izak <kasik83@gmail.com>
2018-03-11 14:48:54 +0100

    js operatory

---
+++ warsztaty/javascript/js/operatory.js
@@ -0,0 +1,60 @@
+// operatory arytmetyczne
+var liczba1 = 20;
+var liczba2 = 4;
+
+var wynik = liczba1 + liczba2;
+
+wynik = liczba1 - liczba2;
+
+wynik = liczba1 * liczba2;
+
+wynik = liczba1 / liczba2;
+
+// modulo - reszta z dzielenia
+wynik = liczba1 % liczba2;
+
+// inkrementacja
+liczba2++; // liczba2 = liczba2 + 1
+
+// dekrementacja
+liczba1--; // liczba1 = liczba1 - 1
+
+
+
+// operatory przypisania
+var liczba3 = 200;
+var liczba4 = 100;
+var liczba5 = 15;
+var liczba6 = 40;
+var liczba7 = 7;
+
+liczba3 += 200; // liczba3 = liczba3 + 200
+liczba4 -= 50; // liczba4 = liczba4 - 50
+liczba5 *= 2; // liczba5 = liczba5 * 2
+liczba6 /= 2; // liczba6 = liczba6 / 2
+liczba7 %= 2; // liczba7 = liczba7 % 2
+
+//console.log(liczba7);
+
+// operatory porównania
+var liczba8 = 10;
+var liczba9 = 10;
+
+if (liczba8 <= liczba9) {
+    console.log('Warunek spełniony');
+} else {
+    console.log('Warunek nie spełniony, czyli false');
+}
+
+
+
+// operatory logiczne
+var liczba10 = 20;
+var liczba11 = 4;
+var czyWiosna = "tak";
+
+if (liczba10 < liczba11 && czyJestWiosna == "tak" ) {
+    console.log('Warunek spełniony');
+} else {
+    console.log('Warunek nie spełniony');
+}
