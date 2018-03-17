e45072f8a11f22fa7d8793f42c0225e2651e3030
Katarzyna Izak <kasik83@gmail.com>
2018-03-17 14:57:34 +0100

    js manipulacje

---
+++ warsztaty/dom/js/manipulacja.js
@@ -0,0 +1,24 @@
+// innerHTML i outerHTML - pobieranie
+var parFirst = document.getElementById('parFirst');
+//console.log(parFirst.innerHTML);
+//console.log(parFirst.outerHTML);
+
+// innerHTML - przypisanie wartości
+var mainHeader = document.querySelector('h1');
+mainHeader.innerHTML = '<span>Nowa zawartość h1 wstawiona w span</span>';
+
+// zmiana styli
+mainHeader.style.color = 'red';
+mainHeader.style.backgroundColor = 'yellow';
+
+// przypisanie klasy - ustawia na nowo atrybuty (istniejace klasy sa usuwane)
+mainHeader.className = 'headingtext';
+
+// pobranie klas elementu
+console.log(mainHeader.className);
+
+// classList - dodaje klasę do istniejących
+mainHeader.classList.add('nowa-klasa');
+
+//  classList - usuwanie klasy
+mainHeader.classList.remove('headingtext');
