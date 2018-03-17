8dac231d0d14a8ff207bfe96d9762141bf7843ef
Katarzyna Izak <kasik83@gmail.com>
2018-03-17 10:27:24 +0100

    model dom

---
+++ warsztaty/dom/js/model-dom.js
@@ -0,0 +1,14 @@
+//console.log(document);
+//console.dir(document);
+
+// zwróci element body jako obiekt
+//console.dir(document.body);
+
+// zwróci element html jako obiekt;
+//console.log(document.activeElement);
+
+// Obiekt window
+//console.log(window);
+//console.log(window.innerWidth); // szer. okna przeglądarki
+//console.log(window.innerHeight); // wys. okna przeglądarki
+console.log(window.document);
