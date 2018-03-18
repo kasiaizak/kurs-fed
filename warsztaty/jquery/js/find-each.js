8ede2460af3ad90fd429c7b83dbdd443897059e4
Katarzyna Izak <kasik83@gmail.com>
2018-03-18 13:55:57 +0100

    jQuery find i each

---
+++ warsztaty/jquery/js/find-each.js
@@ -0,0 +1,14 @@
+// Metoda find wyszukuje potomków selektora. W poniższym przykładzie poszuka paragrafów znajdujących się w div. Metoda find może wyszukiwać po tagum klasie, id, atrybucie
+$('div').find('p').css({
+    fontSize: '30px',
+    color: 'blue',
+});
+
+// Metoda each - jako parametr przyjmuje funkcję zwrotną, któa zostanie wykonana na każdym znalezionym elemencie
+$('p').each(function (index, element) {
+    console.log(index, element);
+    $(this).addClass('newa_class').css({
+        fontSize: '40px',
+        backgroundColor: 'yellow'
+    })
+});
