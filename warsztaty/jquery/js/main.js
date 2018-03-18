babf0fab6466fb7cd27707194c2a404090f5e982
Katarzyna Izak <kasik83@gmail.com>
2018-03-18 11:38:39 +0100

    jQuery start

---
+++ warsztaty/jquery/js/main.js
@@ -0,0 +1,15 @@
+// document.ready gwarantuje, że skrypty wywołają się w momencie gdy cały model DOM jest już załadowany (poniżej skrócony zapis document.ready)
+$(function () {
+    /*$('.title').css({
+        color: 'red',
+        backgroundColor: 'yellow',
+    })*/
+    /*$('h2').css({
+        color: 'red',
+        backgroundColor: 'yellow',
+    })*/
+    $('[class = "title"]').css({
+        color: 'red',
+        backgroundColor: 'yellow',
+    })
+});
