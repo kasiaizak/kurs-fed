a00c2e543ea58012d9cebaafc1db4820edc192ea
Katarzyna Izak <kasik83@gmail.com>
2018-03-19 16:02:16 +0100

    zadanie domowe js - suma i iloczyn

---
+++ zadania-domowe/js-suma-iloczyn/js/suma-iloczyn.js
@@ -0,0 +1,13 @@
+function sumailoczyn() {
+    var tablica = [1, 2, 3, 4, 5, 6];
+    var dlugoscTablicy = tablica.length;
+    var suma = 0;
+    var iloczyn = 1;
+    for (var i = 0; i < dlugoscTablicy; i++) {
+        suma += tablica[i];
+        iloczyn *= tablica[i];
+    }
+    console.log('Suma podanych liczb wynosi: ' + suma + '\nIloczyn podanych liczb wynosi: ' + iloczyn);
+}
+
+sumailoczyn();
