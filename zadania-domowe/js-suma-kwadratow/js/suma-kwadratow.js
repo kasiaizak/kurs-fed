80b36ab43d22431a4ce2e5fda5190b4d0f39200a
Katarzyna Izak <kasik83@gmail.com>
2018-03-19 18:01:44 +0100

    zadanie domowe js - suma kwadratów

---
+++ zadania-domowe/js-suma-kwadratow/js/suma-kwadratow.js
@@ -0,0 +1,11 @@
+function sumakwadratow() {
+    var tablica = [1, 2, 3, 4, 5];
+    var dlugoscTablicy = tablica.length;
+    var suma = 0;
+    for (var i = 0; i < dlugoscTablicy; i++) {
+        suma += Math.pow( tablica[i], 2);
+    }
+    console.log('Suma kwadratów podanych liczb wynosi: ' + suma);
+}
+
+sumakwadratow()
