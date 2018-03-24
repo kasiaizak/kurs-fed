8b99e6464b5dd9a601704089c1cadb7ff7ad188c
Katarzyna Izak <kasik83@gmail.com>
2018-03-24 09:40:14 +0100

    zd jQuery - suma wyplat (liczy sume - trzeba podpiąć)

--- zadania-domowe/jquery-suma-wyplat/js/suma-wyplat.js
+++ zadania-domowe/jquery-suma-wyplat/js/suma-wyplat.js
@@ -4,17 +4,11 @@ $(function () {
         var wyplata = document.getElementsByClassName('salary');
         var iloscWyplat = wyplata.length;
         for (var i = 0; i < iloscWyplat; i++) {
-            suma += wyplata[i].innerText;
+            suma += parseInt(wyplata[i].innerText); // zmienia stringa na integer
         }
         //$('#sum').empty;
         //$('#sum').text(suma);
         //console.log(document.getElementsByClassName('salary')[1].innerText);
-        console.log(wyplata.length);
-        console.log(wyplata[0].innerText);
-        console.log(wyplata[1].innerText);
-        console.log(wyplata[2].innerText);
-        console.log(wyplata[3].innerText);
-        console.log(wyplata[4].innerText);
         console.log(suma);
     });
 });
