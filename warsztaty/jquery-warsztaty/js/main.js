566d3ae35375517832d6e078af1119f60f5364a1
Katarzyna Izak <kasik83@gmail.com>
2018-03-24 15:55:56 +0100

    warsztaty jQuery - slajder (usprawnienia)

--- warsztaty/jquery-warsztaty/js/main.js
+++ warsztaty/jquery-warsztaty/js/main.js
@@ -10,7 +10,7 @@ slideShow.css({
 
 // ustawienie szerokości i pozycjonowanie wewnętrznych kontenerów
 slideShow.find('.single-slide').each(function (index) {
-    $(this).css({
+    $(this).css({ //this - aktualny slajd na którym się znajdujemy
         width: slideWidth + '%',
         marginLeft: slideWidth * index + '%'
     });
@@ -27,12 +27,13 @@ $('.prev-slide').on('click', function () {
     slide(slideIndex - 1);
 });
 
+// funkcja przesuwająca slajdy
 function slide(newSlideIndex) {
     if (newSlideIndex < 0  || newSlideIndex > slideCount - 1) {
         return;
     }
     
-    var slideCaption = $('.slide-caption');
+    var slideCaption = $('.slide-caption').eq(newSlideIndex); //eq... opcjonalne - wybierze tylko aktualny opis
     var marginLeft = newSlideIndex * -100 + '%';
     
     slideCaption.hide();
