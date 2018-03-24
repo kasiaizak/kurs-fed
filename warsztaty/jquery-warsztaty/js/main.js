59ac165b1faca79189ef9dd5d2b8f22da2bf4d85
Katarzyna Izak <kasik83@gmail.com>
2018-03-24 14:47:11 +0100

    warsztaty jQuery - działający slider

--- warsztaty/jquery-warsztaty/js/main.js
+++ warsztaty/jquery-warsztaty/js/main.js
@@ -18,9 +18,31 @@ slideShow.find('.single-slide').each(function (index) {
 
 // przypisywanie zdarzeń do przycisków nawigacji
 $('.next-slide').on('click', function () {
-    console.log('klik naprzód');
+    //console.log('klik naprzód');
+    slide(slideIndex + 1);
 });
 
 $('.prev-slide').on('click', function () {
-    console.log('klik wstecz');
+    //console.log('klik wstecz');
+    slide(slideIndex - 1);
 });
+
+function slide(newSlideIndex) {
+    if (newSlideIndex < 0  || newSlideIndex > slideCount - 1) {
+        return;
+    }
+    
+    var slideCaption = $('.slide-caption');
+    var marginLeft = newSlideIndex * -100 + '%';
+    
+    slideCaption.hide();
+    
+    slideShow.animate(
+        {'margin-left': marginLeft},
+        800,
+        function () {
+            slideIndex = newSlideIndex;
+            slideCaption.fadeIn();
+        }
+    );  
+};
