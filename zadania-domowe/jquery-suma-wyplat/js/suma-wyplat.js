5ca6740a120f7a555ff55329dc49053e52637f3f
Katarzyna Izak <kasik83@gmail.com>
2018-03-24 13:30:29 +0100

    zd jQuery - suma wypłat (zakończone)

--- zadania-domowe/jquery-suma-wyplat/js/suma-wyplat.js
+++ zadania-domowe/jquery-suma-wyplat/js/suma-wyplat.js
@@ -6,9 +6,6 @@ $(function () {
         for (var i = 0; i < iloscWyplat; i++) {
             suma += parseInt(wyplata[i].innerText); // zmienia stringa na integer
         }
-        //$('#sum').empty;
-        //$('#sum').text(suma);
-        //console.log(document.getElementsByClassName('salary')[1].innerText);
-        console.log(suma);
+        document.getElementById('sum').innerHTML = suma;
     });
 });
