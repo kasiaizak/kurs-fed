57991b929f44d8046f697c5448cc3fd46fe21418
Katarzyna Izak <kasik83@gmail.com>
2018-03-25 15:32:46 +0200

    poprawione komentarze

--- warsztaty/ajax/js/ajax.js
+++ warsztaty/ajax/js/ajax.js
@@ -1,40 +1,46 @@
+// wywołujemy naszą funkcję ajax(), zdefiniowaną poniżej, przekazując adres, metodę, i funkcję, która zwrócone dane zaloguje do konsoli :) 
 ajax('GET', 'http://echo.jsontest.com/userId/108/userName/Akademia108/userURL/akademia108.pl', function (data) {
     console.log(data);
 })
 
+// definicja funkcji ajax
 function ajax(method, url, callback) {
-    // żądanie http
+    // utworzenie obiektu żądania http
     var httpRequest = new XMLHttpRequest();
 
-    // otwarcie połączenia
+    // otwarcie połączenia - https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/open
     httpRequest.open(method, url);
 
-    // jeśli status połączenia zmienony (sprawdza status)
+    // jeśli status połączenia został zmieniony -> httpReq.readyState
+    // 0: połączenie nie nawiązane,
+    // 1: połączenie nawiązane,
+    // 2: żądanie odebrane,
+    // 3: przetwarzanie,
+    // 4: dane zwrócone i gotowe do użycia.
     httpRequest.onreadystatechange = function () {
-        // https://pl.wikipedia.org/wiki/XMLHttpRequest - readyState
-        // jeżeli 4: dane zwrócone i gotowe do użycia
+        // jeżeli 4: dane zwrócone i gotowe do użycia - https://pl.wikipedia.org/wiki/XMLHttpRequest
         if (httpRequest.readyState == 4) {
-            // https://pl.wikipedia.org/wiki/Kod_odpowiedzi_HTTP - status
-            // sprawdza kod statusu połączenia HTTP
+            // sprawdź kod statusu połączenia - https://pl.wikipedia.org/wiki/Kod_odpowiedzi_HTTP
             if (httpRequest.status == 200) {
                 
-                // dane zwrócone w stringu
                 //debugger;
+                // dane zwrócone w string-u
                 var returnData = httpRequest.responseText;
                 
-                // zmiana danych na format JSONa
-                //var returnData = JSON.parse(httpRequest.responseText);
+                // zmiana formatu dancy do obiektu JavaScript (można odnoić się do właściwości po kropce, np. jsonObj.userName)
+                var jsonObj = JSON.parse(httpRequest.responseText);
 
-                // wywołanie f-cji zwrotnej (callback), przekazanej do metody z zwenątrz
-                callback(returnData);
+                // wywołanie funkcji zwrotnej (callback), przekazanej do metody z zwenątrz
+                //callback(returnData);
+                callback(jsonObj);
 
-                // zeruje obiekt, aby nie utrzymywać niepotrzebnego już połączenia z serwerem
+                // zeruj obiekt, aby nie utrzymywać niepotrzebnego już połączenia z serwerem
                 httpRequest = null;
             }
         }
     }
     //debugger;
     
-    // wysyłanie danych do serwera
+    // wysłanie danych do serwera
     httpRequest.send();
 }
