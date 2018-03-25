eb7e6f91276525640b0638e63879f13c61268864
Katarzyna Izak <kasik83@gmail.com>
2018-03-25 11:40:44 +0200

    dodano komentarze

--- warsztaty/ajax/js/ajax.js
+++ warsztaty/ajax/js/ajax.js
@@ -3,24 +3,38 @@ ajax('GET', 'http://echo.jsontest.com/userId/108/userName/Akademia108/userURL/ak
 })
 
 function ajax(method, url, callback) {
+    // żądanie http
     var httpRequest = new XMLHttpRequest();
 
+    // otwarcie połączenia
     httpRequest.open(method, url);
 
+    // jeśli status połączenia zmienony (sprawdza status)
     httpRequest.onreadystatechange = function () {
+        // https://pl.wikipedia.org/wiki/XMLHttpRequest - readyState
+        // jeżeli 4: dane zwrócone i gotowe do użycia
         if (httpRequest.readyState == 4) {
+            // https://pl.wikipedia.org/wiki/Kod_odpowiedzi_HTTP - status
+            // sprawdza kod statusu połączenia HTTP
             if (httpRequest.status == 200) {
                 
+                // dane zwrócone w stringu
                 //debugger;
                 var returnData = httpRequest.responseText;
+                
+                // zmiana danych na format JSONa
+                //var returnData = JSON.parse(httpRequest.responseText);
 
+                // wywołanie f-cji zwrotnej (callback), przekazanej do metody z zwenątrz
                 callback(returnData);
 
+                // zeruje obiekt, aby nie utrzymywać niepotrzebnego już połączenia z serwerem
                 httpRequest = null;
             }
         }
     }
     //debugger;
     
+    // wysyłanie danych do serwera
     httpRequest.send();
 }
