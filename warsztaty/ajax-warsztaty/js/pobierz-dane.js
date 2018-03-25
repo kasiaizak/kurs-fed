057f1d5a90b46cb650710b7e5d7842e45d735906
Katarzyna Izak <kasik83@gmail.com>
2018-03-25 11:10:47 +0200

    warsztaty Ajax - js

---
+++ warsztaty/ajax-warsztaty/js/pobierz-dane.js
@@ -0,0 +1,51 @@
+var przycisk = document.getElementById('przycisk');
+
+przycisk.addEventListener('click',function(event) {
+    event.preventDefault();
+    ajax("GET", "http://echo.jsontest.com/userId/108/userName/Akademia108/userURL/akademia108.pl", pobierzDane)
+});
+
+function pobierzDane(dane){
+    var paragrafId = document.createElement('p');
+    var textId = document.createTextNode('userId: ' + dane.userId);
+    paragrafId.appendChild(textId);
+    
+    var paragrafName = document.createElement('p');
+    var textName = document.createTextNode('username: ' + dane.userName);
+    paragrafName.appendChild(textName);
+    
+    var paragrafUrl = document.createElement('p');
+    var textUrl = document.createTextNode('userURL: ' + dane.userURL);
+    paragrafUrl.appendChild(textUrl);
+    
+    var przerywnik = document.createElement('p');
+    var cialoPrzerywnika = document.createTextNode('---------------------');
+    przerywnik.appendChild(cialoPrzerywnika);
+    
+    document.body.appendChild(paragrafId);
+    document.body.appendChild(paragrafName);
+    document.body.appendChild(paragrafUrl);
+    document.body.appendChild(przerywnik);
+    //console.log(dane.userId);
+}
+
+function ajax(method, url, callback) {
+    var httpRequest = new XMLHttpRequest();
+
+    httpRequest.open(method, url);
+
+    httpRequest.onreadystatechange = function () {
+        if (httpRequest.readyState == 4) {
+            if (httpRequest.status == 200) {
+                var returnData = JSON.parse(httpRequest.responseText);
+                //debugger;
+
+                callback(returnData);
+
+                httpRequest = null;
+            }
+        }
+    }
+    
+    httpRequest.send();
+}
