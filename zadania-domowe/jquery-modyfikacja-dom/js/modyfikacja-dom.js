7b9de1e6eac6d9ad96b198e96bf1c6201b245098
Katarzyna Izak <kasik83@gmail.com>
2018-04-13 15:22:25 +0200

    zadanie domowe - jQuery - modyfikacja DOM

--- zadania-domowe/jquery-modyfikacja-dom/js/modyfikacja-dom.js
+++ zadania-domowe/jquery-modyfikacja-dom/js/modyfikacja-dom.js
@@ -1,8 +1,12 @@
-$(function () {
+ $(function () {
     $('#ostatni-element').on('click', function (event) {
-        
+        var ostatni = $('p:last').html();
+        $('p:last').remove();
+        $('p:first').before('<p>' + ostatni + '</p>');
     });
     $('#pierwszy-element').on('click', function (event) {
-        
+        var pierwszy = $('p:first').html(); 
+        $('p:first').remove();
+        $('p:last').after(('<p>' + pierwszy + '</p>'))
     });
 });
