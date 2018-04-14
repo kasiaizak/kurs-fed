f9a86dc095affaae868f6c17b3ba59a9175f1403
Katarzyna Izak <kasik83@gmail.com>
2018-04-14 10:18:47 +0200

    zmiana html na text

--- zadania-domowe/jquery-modyfikacja-dom/js/modyfikacja-dom.js
+++ zadania-domowe/jquery-modyfikacja-dom/js/modyfikacja-dom.js
@@ -1,11 +1,11 @@
  $(function () {
     $('#ostatni-element').on('click', function (event) {
-        var ostatni = $('p:last').html();
+        var ostatni = $('p:last').text();
         $('p:last').remove();
         $('p:first').before('<p>' + ostatni + '</p>');
     });
     $('#pierwszy-element').on('click', function (event) {
-        var pierwszy = $('p:first').html(); 
+        var pierwszy = $('p:first').text(); 
         $('p:first').remove();
         $('p:last').after(('<p>' + pierwszy + '</p>'))
     });
