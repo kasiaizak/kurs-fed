8424cf39a50587feefebbad96387f1d5f43bc272
Katarzyna Izak <kasik83@gmail.com>
2018-04-23 09:34:55 +0200

    warsztaty do dokonczenia

---
+++ warsztaty/dom-warsztaty/warsztat-3/js/walidator.js
@@ -0,0 +1,15 @@
+function ustawTlo() {
+    var par = document.getElementsByTagName('p');
+    par[0].style.backgroundColor = 'red';
+    par[1].style.backgroundColor = 'yellow';
+}
+
+function pobierzImie(event) {
+    event.preventDefault();
+    var dane = document.getElementsByTagName('input');
+    console.log(dane[0].value + ' ' + dane[1].value);
+    /*var inputs = document.getElementsByTagName('input');
+    var imie = inputs[0].value;
+    var nazwisko = inputs[1].value;
+    console.log(imie + ' ' + nazwisko);*/
+}
